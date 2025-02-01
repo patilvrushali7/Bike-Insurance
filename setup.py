e9f3cb5837ff594ab971690a43fbb2974dc9d861
vrushali <vrushalip037@gmail.com>
2025-02-01 16:15:54 +0530

    Project structure is ready,venv setupt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vrushali",
+    author_email="vrushalip037@gmail.com",
+    packages=find_packages()
+)
